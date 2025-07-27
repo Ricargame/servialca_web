cb023cd6ae2a341d6540fec07ca0655e09358dd8
Ricargame <ricardofenomeno13@gmail.com>
2025-07-27 11:16:18 -0400

    0422

--- frontend/src/components/administracion/modalRcv.js
+++ frontend/src/components/administracion/modalRcv.js
@@ -1803,6 +1803,7 @@ export const ModalRcv = (props) => {
                     >
                       <option value="0414-">0414</option>
                       <option value="0424-">0424</option>
+                      <option value="0422-">0422</option>
                       <option value="0416-">0416</option>
                       <option value="0426-">0426</option>
                       <option value="0412-">0412</option>
